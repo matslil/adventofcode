773fb162d2334d484a00d55b897d626bcd5be6ca
Mats Liljegren <liljegren.mats@gmail.com>
2022-12-02 07:57:32 +0100

    Day 1

---
+++ 1a.rs
@@ -0,0 +1,36 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    // File hosts must exist in current path before this produces output
+    if let Ok(lines) = read_lines(&args[1]) {
+        // Consumes the iterator, returns an (Optional) String
+        let mut total_calories : u32 = 0;
+        let mut calories = Vec::new();
+        for line in lines {
+            if let Ok(entry) = line {
+                let entry_trimmed = entry.trim();
+                if entry_trimmed == "" {
+                    calories.push(total_calories);
+                    total_calories = 0;
+                } else {
+                    total_calories +=  entry_trimmed.parse::<u32>().unwrap();
+                }
+            }
+        }
+        calories.sort();
+        calories.reverse();
+        print!("{}", calories[0]);
+    }
+}
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
