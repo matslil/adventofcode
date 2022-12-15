fe8378ce0280d442f1be78e0da9a93d8ab74d6d2
Mats Liljegren <liljegren.mats@gmail.com>
2022-12-15 09:16:35 +0100

    Day 15 - Not finished

---
+++ day-15/src/bin/a.rs
@@ -0,0 +1,80 @@
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+use std::env;
+use scanf::sscanf;
+use core::ops::Sub;
+
+type PosType = i32;
+
+type Pos = (PosType, PosType);
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let input = args.get(1).expect("Expected a file name as argument 1");
+    let inspect = args.get(2).expect("Expected y value as argument 2").parse::<u32>().expect("Not a valid integer");
+    println!("{}", get_answer(input, &inspect));
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Sensor {
+    pub pos: Pos,
+    pub beacon: Pos,
+}
+
+fn get_answer(file: &str, inspect_y: &u32) -> u32 {
+    let mut sensors = Vec::<Sensor>::new();
+    for line in BufReader::new(File::open(file).unwrap()).lines().map(|x| x.unwrap()) {
+        sensors.push(parse_line(&line));
+    }
+    for sensor in sensors {
+    }
+    return 26;
+}
+
+fn abs_difference<T>(x: T, y: T) -> T
+where
+    T: Sub<Output = T> + Ord,
+{
+    if x < y {
+        y - x
+    } else {
+        x - y
+    }
+}
+
+fn manhattan(pos1: Pos, pos2: Pos) -> PosType
+{
+    abs_difference(pos2.0, pos1.0) + abs_difference(pos2.1, pos1.1)
+}
+
+fn parse_line(line: &str) -> Sensor {
+    let mut sensor_x: i32 = 0;
+    let mut sensor_y: i32 = 0;
+    let mut beacon_x: i32 = 0;
+    let mut beacon_y: i32 = 0;
+    if ! sscanf!(line, "Sensor at x={}, y={}: closest beacon is at x={}, y={}",
+                sensor_x, sensor_y, beacon_x, beacon_y).is_ok() {
+        panic!("{}: Malformed input", line);
+    }
+    Sensor {
+        pos: (sensor_x, sensor_y),
+        beacon: (beacon_x, beacon_y)
+    }
+}
+
+#[cfg(test)]
+#[test]
+fn test_manhattan() {
+    assert_eq!(manhattan((1,1), (1,1)), 0);
+    assert_eq!(manhattan((5,4), (3,2)), 4);
+    assert_eq!(manhattan((1,1), (0,3)), 3);
+    assert_eq!(manhattan((-1, -1), (-2, 1)), 3);
+}
+
+#[test]
+fn test_input() {
+    const INPUT_FILE: &str = "test";
+    const INPUT_Y: u32 = 10;
+
+    assert_eq!(get_answer(INPUT_FILE, &INPUT_Y), 26);
+}
