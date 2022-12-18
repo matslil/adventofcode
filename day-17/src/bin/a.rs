8f21f0ba065449e729e4658ada77baeaa1e5fb65
Mats Liljegren <liljegren.mats@gmail.com>
2022-12-18 13:22:06 +0100

    day-17: Passed test, but does not run input

--- day-17/src/bin/a.rs
+++ day-17/src/bin/a.rs
@@ -1,12 +1,13 @@
+use pathfinding::matrix::Matrix;
 use std::fs::File;
-use std::io::{BufReader, BufRead};
+use std::io::{BufRead, BufReader};
 
 const NR_ROCKS: usize = 2022;
 const SHAFT_WIDTH: usize = 7;
 
-type RockType = Vec<Vec<bool>>;
+type RockType = Matrix<bool>;
 
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, Clone)]
 enum Rock {
     Minus(RockType),
     Plus(RockType),
@@ -15,15 +16,25 @@ enum Rock {
     Dot(RockType),
 }
 
-type Board = Vec<[bool; 7]>;
+type Board = Matrix<bool>;
 
 type Pos = (usize, usize);
 
-#[derive(Debug, Clone, Copy, Default)]
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq)]
 enum Dir {
     #[default]
     Left,
-    Right
+    Right,
+}
+
+fn rock_matrix(rock: &Rock) -> &RockType {
+    match rock {
+        Rock::Minus(entry)
+        | Rock::Plus(entry)
+        | Rock::BackL(entry)
+        | Rock::I(entry)
+        | Rock::Dot(entry) => entry,
+    }
 }
 
 fn main() {
@@ -32,122 +43,185 @@ fn main() {
 }
 
 fn get_answer(file: &str) -> usize {
-    let dir_iter = parse_line(&BufReader::new(File::open(file).unwrap()).lines().map(|x| x.unwrap()).nth(1).unwrap()).into_iter().cycle();
+    let lines = &BufReader::new(File::open(file).unwrap())
+        .lines()
+        .map(|x| x.unwrap())
+        .collect::<Vec<_>>();
+    let mut dir_iter = parse_line(&lines[0]).into_iter().cycle();
+    // Note: The rocks below are up-side down
     let rocks: Vec<Rock> = vec![
-        Rock::Minus(vec![
-            vec![ true, true, true, true ],
-        ]),
-        Rock::Plus(vec![
-            vec![ false, true , false ],
-            vec![ true , true , true  ],
-            vec![ false, true , false ],
-        ]),
-        Rock::BackL(vec![
-            vec![ false, false, true  ],
-            vec![ false, false, true  ],
-            vec![ true , true , true  ],
-        ]),
-        Rock::I(vec![
-            vec![ true ],
-            vec![ true ],
-            vec![ true ],
-            vec![ true ],
-        ]),
-        Rock::Dot(vec![
-            vec![ true, true ],
-            vec![ true, true ],
-        ]),
+        Rock::Minus(RockType::from_vec(1, 4,vec![
+                true, true, true, true
+            ]).unwrap()),
+        Rock::Plus(
+            RockType::from_vec(3, 3, vec![
+                false, true , false,
+                true , true , true ,
+                false, true , false
+            ]).unwrap()),
+        Rock::BackL(
+            RockType::from_vec(3, 3, vec![
+                true , true , true ,
+                false, false, true ,
+                false, false, true ,
+            ]).unwrap()),
+        Rock::I(RockType::from_vec(4, 1, vec![
+                true,
+                true,
+                true,
+                true
+            ]).unwrap()),
+        Rock::Dot(RockType::from_vec(2, 2, vec![
+                true, true,
+                true, true
+            ]).unwrap()),
     ];
-    let rocks_iter = rocks.into_iter().cycle();
-    let rock = if let Some(try_rock) = rocks_iter.next() {
-            try_rock
-        } else {
-            panic!("Rock iterator exhausted");
-        };
-    let dir = if let Some(try_dir) = dir_iter.next() {
-            try_dir
-        } else {
-            panic!("Dir iterator exhausted");
-        };
-    let mut board: Board = Default::default();
-    let mut pos  = add_new_rock(&board, &rock);
+    let mut rocks_iter = rocks.into_iter().cycle();
+    let mut rock = if let Some(try_rock) = rocks_iter.next() {
+        try_rock
+    } else {
+        panic!("Rock iterator exhausted");
+    };
+    let mut dir = if let Some(try_dir) = dir_iter.next() {
+        try_dir
+    } else {
+        panic!("Dir iterator exhausted");
+    };
+    let mut board = Board::new(0, 7, false);
+    let mut pos = add_new_rock(&board, &rock);
     let mut do_move_down = false;
-    for _ in 0..NR_ROCKS {
-        if do_move_down {
-            if ! move_down(&board, &rock, &mut pos) {
-                merge_rock_into_board(&mut board, &rock, &pos);
-                if let Some(try_rock) = rocks_iter.next() {
-                    rock = try_rock;
+    println!("{:?} {:?}", rock, pos);
+    for rock_nr in 0..NR_ROCKS {
+        println!("---- Rock {} ----", rock_nr);
+        loop {
+            if do_move_down {
+                if !move_down(&board, &rock, &mut pos) {
+                    merge_rock_into_board(&mut board, &rock, &pos);
+                    if let Some(try_rock) = rocks_iter.next() {
+                        rock = try_rock;
+                    } else {
+                        panic!("Rock iterator exhausted");
+                    }
+                    pos = add_new_rock(&board, &rock);
+                    println!("{:?} {:?}", rock, pos);
+                    do_move_down = false;
+                    break;
                 } else {
-                    panic!("Rock iterator exhausted");
+                    println!("V {:?}", pos);
                 }
-                pos = add_new_rock(&board, &rock);
-            }
-        } else {
-            move_sideways(&board, &rock, &mut pos, dir);
-            if let Some(try_dir) = dir_iter.next() {
-                dir = try_dir;
             } else {
-                panic!("Dir iterator exhausted");
+                move_sideways(&board, &rock, &mut pos, dir);
+                 println!(
+                    "{} {:?}",
+                    if dir == Dir::Left {
+                        '<'
+                    } else {
+                        '>'
+                    },
+                    pos
+                );
+                if let Some(try_dir) = dir_iter.next() {
+                    dir = try_dir;
+                } else {
+                    panic!("Dir iterator exhausted");
+                }
             }
+            do_move_down = !do_move_down;
         }
     }
-    board.len()
+    board.rows
 }
 
 fn parse_line(line: &str) -> Vec<Dir> {
-    line.chars().map(|x| if x == '<' { Dir::Left } else { Dir::Right }).collect::<Vec<_>>()
+    line.chars()
+        .map(|x| if x == '<' { Dir::Left } else { Dir::Right })
+        .collect::<Vec<_>>()
 }
 
 fn add_new_rock(board: &Board, rock: &Rock) -> Pos {
-    (2, board.len() + 3 + rock.len())
+    (2, board.rows + 2 + rock_matrix(rock).rows)
 }
 
 fn merge_rock_into_board(board: &mut Board, rock: &Rock, pos: &Pos) {
-    for _ in 0..rock.len() {
+    for _ in board.rows..=pos.1 {
         add_blank_row_to_board(board);
     }
-    for (rock_y, rock_row) in rock.enumerate() {
-        for (rock_x, rock_cell) in rock_row.enumerate() {
-            board[rock_y + pos.1][rock_x + pos.0] = rock_cell;
+    for (rock_row_idx, rock_row) in rock_matrix(rock).iter().enumerate() {
+        for (rock_col_idx, rock_cell) in rock_row.iter().enumerate() {
+            let rock_x = rock_col_idx + pos.0;
+            let rock_y = pos.1 - (rock_matrix(rock).rows - rock_row_idx - 1);
+            if *rock_cell {
+                *board.get_mut((rock_y, rock_x)).unwrap() = true;
+            }
         }
+    }
+    print_board(board);
+}
+
+fn print_board(board: &Board) {
+    let mut matrix = board.clone();
+    matrix.flip_ud();
+    for (row_idx, row) in matrix.iter().enumerate() {
+        print!("{:>4} ", board.rows - row_idx - 1);
+        for cell in row {
+            print!("{}", if *cell { '#' } else { '.' });
+        }
+        println!("");
     }
 }
 
 fn add_blank_row_to_board(board: &mut Board) {
-    board.push([false.repeat(7)]);
+    board
+        .extend(&[false, false, false, false, false, false, false])
+        .unwrap();
 }
 
 fn move_down(board: &Board, rock: &Rock, pos: &mut Pos) -> bool {
-    let try_pos = (pos.0, pos.1 -1);
-    if try_pos.1 == 0 {
-        false
-    } else if check_collision(board, rock, try_pos) {
+    if pos.1 == 0 {
         false
     } else {
-        pos = try_pos;
-        true
+        let try_pos = (pos.0, pos.1 - 1);
+        if check_collision(board, rock, try_pos) {
+            false
+        } else {
+            *pos = try_pos;
+            true
+        }
     }
 }
 
 fn move_sideways(board: &Board, rock: &Rock, pos: &mut Pos, dir: Dir) -> bool {
-    let try_pos = (if dir == Dir::Left { pos.0 - 1 } else { pos.0 + 1 }, pos.1);
-    if try_pos.0 == 0 || (try_pos.0 + rock[0].len()) >= (SHAFT_WIDTH - 1) {
+    let rock_width = rock_matrix(rock).columns;
+    if dir == Dir::Left && pos.0 == 0 {
         false
-    } else if check_collision(board, rock, try_pos) {
+    } else if dir == Dir::Right && (pos.0 + rock_width) >= SHAFT_WIDTH {
         false
     } else {
-        pos = try_pos;
-        true
+        let try_pos = (
+            if dir == Dir::Left {
+                pos.0 - 1
+            } else {
+                pos.0 + 1
+            },
+            pos.1,
+        );
+        if check_collision(board, rock, try_pos) {
+            false
+        } else {
+            *pos = try_pos;
+            true
+        }
     }
 }
 
-fn check_collision(board: &Board, rock: &Rock, pos: Pos) {
-    for (board_y, board_row) in board.enumerate() {
-        for (board_x, board_cell) in board_row.enumerate() {
-            for (rock_y, rock_row) in rock.enumerate() {
-                for (rock_x, rock_cell) in rock_row.enumerate() {
-                    if rock_cell {
+fn check_collision(board: &Board, rock: &Rock, pos: Pos) -> bool {
+    for (rock_row_idx, rock_row) in rock_matrix(rock).iter().enumerate() {
+        for (rock_col_idx, rock_cell) in rock_row.iter().enumerate() {
+            if *rock_cell {
+                let rock_x = rock_col_idx + pos.0;
+                let rock_y = pos.1 - (rock_matrix(rock).rows - rock_row_idx - 1);
+                if let Some(board_cell) = board.get((rock_y, rock_x)) {
+                    if *board_cell {
                         return true;
                     }
                 }
