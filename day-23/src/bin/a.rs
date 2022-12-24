246b40e47f1ce98d27f07c05d4deb170406f4453
Mats Liljegren <liljegren.mats@gmail.com>
2022-12-24 12:22:06 +0100

    day-23: Done part 1

--- day-23/src/bin/a.rs
+++ day-23/src/bin/a.rs
@@ -2,6 +2,7 @@ use std::fs::File;
 use std::io::{BufReader, BufRead};
 use pathfinding::matrix::Matrix;
 use std::iter;
+use std::collections::HashMap;
 
 type Board = Matrix<bool>;
 
@@ -34,12 +35,13 @@ fn get_answer(file: &str) -> usize {
 
     for _round in 1..=10 {
         board = board_margins(&board);
-        println!("---- {} ----", _round - 1);
+        println!("---- {} {:?} ----", _round - 1, start_dir.clone().next());
         print_board(&board);
-        let mut move_proposals: Vec<((usize, usize),(usize, usize))> = Vec::new();
+        let mut move_proposals: HashMap<(usize, usize),Vec<(usize, usize)>> = HashMap::new();
         for pos in board.items().filter(|item| item.1 == &true).map(|item| item.0).collect::<Vec<_>>() {
             // If no one is next to the elf, do nothing
-            if board.neighbours(pos, true).map(|pos| board.get(pos).unwrap()).fold(false, |acc, &occupied| if acc || occupied { true } else { false }) {
+            if ! board.neighbours(pos, true).map(|pos| board.get(pos).unwrap()).fold(false, |acc, &occupied| if acc || occupied { true } else { false }) {
+                println!("{:?}: Happy at current position", pos);
                 continue;
             }
             let start_at = start_dir.clone();
@@ -55,6 +57,7 @@ fn get_answer(file: &str) -> usize {
                     let check_pos: (usize, usize) = ((pos.0 as isize + delta.0) as usize, (pos.1 as isize + delta.1) as usize);
                     if let Some(occupied) = board.get(check_pos) {
                         if *occupied {
+                            println!("{:?}: Blocked by {:?}", pos, check_pos);
                             has_neighbours = true;
                             break;
                         }
@@ -64,28 +67,28 @@ fn get_answer(file: &str) -> usize {
                     let delta = match dir {
                         Dir::N(item) | Dir::S(item) | Dir::W(item) | Dir::E(item) => item[0]
                     };
-                    move_proposals.push(((pos),((pos.0 as isize + delta.0) as usize, (pos.1 as isize + delta.1) as usize)));
+                    let new_pos = ((pos.0 as isize + delta.0) as usize, (pos.1 as isize + delta.1) as usize);
+                    println!("{:?}: proposes move to {:?}", pos, new_pos);
+                    move_proposals.entry(new_pos).and_modify(|entry| entry.push(pos)).or_insert(vec![pos]);
                     break;
                 }
             }
         }
-        let mut move_proposals_unduplicated = move_proposals.clone();
+        println!("Proposals: {:?}", move_proposals);
         for proposal in &move_proposals {
-            if move_proposals_unduplicated.contains(proposal) {
-                move_proposals_unduplicated.retain(|item| item != proposal);
+            if proposal.1.len() == 1 {
+                let to   = *proposal.0;
+                let from = proposal.1[0];
+                println!("{:?}: Moving to {:?}", from, to);
+                *board.get_mut(from).unwrap() = false;
+                *board.get_mut(to).unwrap() = true;
             }
-        }
-        for proposal in move_proposals {
-            let from = proposal.0;
-            let to = proposal.1;
-            *board.get_mut(from).unwrap() = false;
-            *board.get_mut(to).unwrap() = true;
         }
         start_dir.next().unwrap();
     }
 
     print_board(&board);
-    board.values().fold(0, |acc, occupied| if ! occupied { acc + 1 } else { acc }) - (board.rows * 2) - (board.columns * 2)
+    board.values().fold(0, |acc, occupied| if ! occupied { acc + 1 } else { acc }) - calculate_margins(&board)
 }
 
 fn print_board(board: &Board) {
@@ -144,6 +147,68 @@ fn board_margins(input_board: &Board) -> Board {
     board
 }
 
+fn calculate_margins(board: &Board) -> usize {
+    let mut empty_top: isize = 0;
+    let mut empty_bottom: isize = 0;
+    let mut empty_left: isize = 0;
+    let mut empty_right: isize = 0;
+
+    let mut last_empty_row_from_top: isize = -2;
+    let mut first_empty_row_from_bottom: isize = 0;
+    let mut last_empty_column_from_left: isize = -2;
+    let mut first_empty_column_from_right: isize = 0;
+
+    for (y, row) in board.iter().enumerate() {
+        if row.iter().fold(false, |acc, &entry| if acc || entry { true } else { false }) {
+            if last_empty_row_from_top < 0 {
+                last_empty_row_from_top = y as isize - 1;
+            }
+            first_empty_row_from_bottom = y as isize + 1;
+        }
+    }
+
+    for x in 0..board.columns {
+        let mut found = false;
+        for y in 0..board.rows {
+            if *board.get((y, x)).unwrap() {
+                found = true;
+                break;
+            }
+        }
+        if found {
+            if last_empty_column_from_left < 0 {
+                last_empty_column_from_left = x as isize - 1;
+            }
+            first_empty_column_from_right = x as isize + 1;
+        }
+    }
+
+    println!("{} {} {} {}", last_empty_row_from_top, first_empty_row_from_bottom, last_empty_column_from_left, first_empty_column_from_right);
+
+    if last_empty_row_from_top >= 0 {
+        empty_top = last_empty_row_from_top + 1;
+    }
+    if first_empty_row_from_bottom < board.rows as isize {
+        empty_bottom = board.rows as isize - first_empty_row_from_bottom;
+    }
+    if last_empty_column_from_left >= 0 {
+        empty_left = last_empty_column_from_left + 1;
+    }
+    if first_empty_column_from_right < board.columns as isize {
+        empty_right = board.columns as isize - first_empty_column_from_right;
+    }
+
+    println!("Dimensions (rows x columns): {} x {}", board.rows, board.columns);
+    println!("Top: {}, bottom: {}, left: {}, right: {}", empty_top, empty_bottom, empty_left, empty_right);
+
+    let empty_rows = (empty_top + empty_bottom) as usize;
+    let empty_columns = (empty_left + empty_right) as usize;
+    let total = empty_rows * board.columns + empty_columns * board.rows - empty_rows * empty_columns;
+    println!("{}", total);
+    total
+}
+
+
 fn add_column_right(board: &Board) -> Board {
     let mut new_board = Board::new(board.rows, board.columns + 1, false);
 
@@ -188,6 +253,14 @@ fn add_row_top(board: &Board) -> Board {
 
 mod test {
     use super::*;
+
+#[test]
+    fn test_small_input() {
+        const INPUT_FILE: &str = "test.small";
+        const ANSWER: usize = 25;
+
+        assert_eq!(get_answer(INPUT_FILE), ANSWER);
+    }
 
 #[test]
     fn test_input() {
