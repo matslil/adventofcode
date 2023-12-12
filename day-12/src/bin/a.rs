64da7c5647b5f89c592898fce5da6a1d4722ef86
Mats Liljegren <liljegren.mats@gmail.com>
2023-12-12 09:06:31 +0100

    Day-12: Part 1 does not compile

--- day-12/src/bin/a.rs
+++ day-12/src/bin/a.rs
@@ -3,112 +3,7 @@ use tracing_subscriber::{filter, prelude::*};
 use std::io::{BufRead, BufReader};
 use std::fs::File;
 use std::sync::Arc;
-use itertools::Itertools;
-
-fn print_map(map: &Vec<Vec<bool>>) {
-    let mut map_str: String = String::new();
-
-    for row in map {
-        map_str.push('\n');
-        for cell in row {
-            map_str.push(if *cell { '#' } else { '.' });
-        }
-    }
-    info!("{}", map_str);
-}
-
-fn get_galaxies(file: &str) -> Vec<(usize, usize)> {
-    let map: Vec<Vec<bool>> = BufReader::new(File::open(file).unwrap())
-        .lines()
-        .map(|e| e
-            .unwrap()
-            .chars()
-            .map(|e| if e == '#' { true } else { false })
-            .collect::<Vec<bool>>()
-        )
-        .collect::<Vec<Vec<bool>>>();
-
-    print_map(&map);
-
-    let mut empty_rows: Vec<usize> = Vec::new();
-    let mut empty_cols: Vec<usize> = Vec::new();
-    for (y, row) in map.iter().enumerate() {
-        let mut row_empty = true;
-        for cell in row {
-            if *cell {
-                row_empty = false;
-                break;
-            }
-        }
-        if row_empty {
-            empty_rows.push(y);
-        }
-    }
-    for x in 0..map[0].len() {
-        let mut col_empty = true;
-        for y in 0..map.len() {
-            if map[y][x] {
-                col_empty = false;
-                break;
-            }
-        }
-        if col_empty {
-            empty_cols.push(x);
-        }
-    }
-
-    let mut expanded_map: Vec<Vec<bool>> = Vec::new();
-
-    let mut empty_row: Vec<bool> = Vec::new();
-    empty_row.resize(map[0].len() + empty_cols.len(), false);
-
-    for (y, row) in map.into_iter().enumerate() {
-        if empty_rows.contains(&y) {
-            expanded_map.push(empty_row.clone());
-            expanded_map.push(empty_row.clone());
-        } else {
-            let mut expanded_row: Vec<bool> = Vec::new();
-            for (x, cell) in row.iter().enumerate() {
-                if empty_cols.contains(&x) {
-                    expanded_row.push(false);
-                    expanded_row.push(false);
-                } else {
-                    expanded_row.push(*cell);
-                }
-            }
-            expanded_map.push(expanded_row);
-        }
-    }
-
-    print_map(&expanded_map);
-
-    let mut galaxies: Vec<(usize, usize)> = Vec::new();
-
-    for (y, row) in expanded_map.into_iter().enumerate() {
-        for (x, cell) in row.into_iter().enumerate() {
-            if cell {
-                galaxies.push((x, y));
-            }
-        }
-    }
-
-    galaxies
-}
-
-fn distance(a: (usize, usize), b: (usize, usize)) -> usize {
-    let dist = (if a.0 > b.0 {
-        a.0 - b.0
-    } else {
-        b.0 - a.0
-    }) + (if a.1 > b.1 {
-        a.1 - b.1
-    } else {
-        b.1 - a.1
-    });
-
-    info!("Distance {:?} -> {:?}: {}", a, b, dist);
-    dist
-}
+use std::collections::VecDeque;
 
 fn setup_tracing() {
     let stdout_log = tracing_subscriber::fmt::layer()
@@ -134,18 +29,110 @@ fn setup_tracing() {
 }
 
 fn main() {
-    setup_tracing();
     println!("{:?}", get_answer("input"));
 }
 
-fn get_answer(file: &str) -> usize {
-    let galaxies = get_galaxies(file);
+fn pattern_string(pattern: &Vec<bool>) -> String {
+    let mut string = String::new();
+    for joker in pattern {
+        string.push(match joker {
+            true => '?',
+            false => '#',
+        });
+    }
+    string
+}
 
-    galaxies.into_iter().combinations(2).map(|pair| distance(pair[0], pair[1])).into_iter().sum()
+fn translate_pattern(pattern: Vec<bool>, values: VecDeque<usize>) -> (usize, Vec<bool>, VecDeque<usize>) {
+    let mut count = 0usize;
+
+    info!("translate_pattern({}, {:?}", pattern_string(&pattern), values);
+
+    if values.len() == 0 {
+        info!("translate_pattern() -> 0");
+        return (0, pattern, VecDeque::new());
+    }
+
+    let value = values.pop_front().unwrap();
+
+    if value < pattern.len() {
+        if pattern.into_iter().fold(true, |acc, e| acc && e) {
+            // Cannot do this, since there are non-jokers left
+            //
+            info!("translate_pattern() -> 0");
+            return (0, pattern, values);
+        }
+    }
+
+    if value == pattern.len() {
+        if pattern.into_iter().fold(true, |acc, e| acc && e) {
+            // Cannot do this, since there are non-jokers left
+            return (0, pattern, values);
+        }
+        info!("translate_pattern() -> 1");
+        return (1, Vec::new(), values);
+    }
+
+    for (nr, trial) in pattern.windows(value).enumerate() {
+        let pattern_iter = pattern[(nr+value)..].to_vec().into_iter();
+        let (pattern_count, remaining_pattern, remaining_values) = match pattern_iter.next() {
+            None => continue,
+            Some(c) => if !c {
+                continue
+            } else {
+                translate_pattern(pattern_iter.collect(), values)
+            },
+        };
+        if ! pattern.into_iter().fold(true, |acc, e| acc && e) && values.len() == 0 {
+            count += pattern_count;
+        }
+    }
+
+    info!("translate_pattern() -> {}", count);
+    (count, pattern, values)
+}
+
+fn translate(patterns: Vec<Vec<bool>>, values: VecDeque<usize>) -> usize {
+    let mut count = 0usize;
+    for pattern in patterns {
+        let (pattern_count, remaining_pattern, remaining_values) = translate_pattern(pattern, values);
+        if ! pattern.into_iter().fold(true, |acc, e| acc && e) && values.len() == 0 {
+            count += pattern_count;
+        }
+    }
+    count
+}
+fn get_answer(file: &str) -> usize {
+    setup_tracing();
+    let mut count = 0usize;
+
+    for line in BufReader::new(File::open(file).unwrap())
+        .lines()
+        .map(|e| e.unwrap()) {
+            count += line
+                .split_whitespace()
+                .map(|pair| (pair[0], pair[1]
+                        .into_iter()
+                        .split(",")
+                        .map(|v| v
+                            .parse::<usize>()
+                            .collect::<Vec<usize>>())))
+                .fold(0, |acc, e| acc + translate(
+                        e.0
+                        .into_iter()
+                        .split(".")
+                        .filter(|s| s != "")
+                        .map(|s| s
+                            .chars()
+                            .map(|c| c == '?')
+                            .collect::<Vec<bool>>())
+                        .collect::<Vec<Vec<bool>>>(),
+                        e.1.into()));
+        }
+    count
 }
 
 #[test]
 fn test() {
-    setup_tracing();
-    assert_eq!(374, get_answer("test"));
+    assert_eq!(21, get_answer("test"));
 }
