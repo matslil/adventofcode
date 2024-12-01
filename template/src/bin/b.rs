4edf575c880c1c2110b875d20e99d73207c25114
Mats Liljegren <liljegren.mats@gmail.com>
2024-12-01 09:35:54 +0100

    Updated template to use tracing instead of flexi-logger

--- template/src/bin/b.rs
+++ template/src/bin/b.rs
@@ -1,20 +1,43 @@
 // use flexi_logger;
 // use log::{info, warn};
 
+use tracing_subscriber::{filter, prelude::*};
+use std::{fs::File, sync::Arc};
+use tracing::{info, debug, warn};
+
 fn setup_tracing() {
-    flexi_logger::Logger::try_with_str("info").unwrap()
-    .log_to_file(flexi_logger::FileSpec::default())         // write logs to file
-    .duplicate_to_stderr(flexi_logger::Duplicate::Warn)     // print warnings and errors also to the console
-    .start().unwrap();
+    let stdout_log = tracing_subscriber::fmt::layer()
+        .pretty();
+
+    // A layer that logs events to a file.
+    let file = File::create("debug.log");
+    let file = match file  {Ok(file) => file,Err(error) => panic!("Error: {:?}",error),};
+    let debug_log = tracing_subscriber::fmt::layer()
+        .with_writer(Arc::new(file));
+
+    tracing_subscriber::registry()
+        .with(
+            stdout_log
+                // Add an `INFO` filter to the stdout logging layer
+                .with_filter(filter::LevelFilter::INFO)
+                // Combine the filtered `stdout_log` layer with the
+                // `debug_log` layer, producing a new `Layered` layer.
+                .and_then(debug_log)
+        )
+        .init();
+
+    debug!("Debug message example");
+    info!("Info message example");
+    warn!("Warning message example");
 }
 
 fn main() {
     setup_tracing();
-    log::info!("{:?}", get_answer("input"));
+    info!("{:?}", get_answer("input"));
 }
 
 fn get_answer(file: &str) -> usize {
-    log::info!("File: {:?}", file);
+    info!("{:?}: Using as input data", file);
     return 1;
 }
 
