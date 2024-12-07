07dac9f2b160dfce376476d6390d5cba3f872a66
Mats Liljegren <liljegren.mats@gmail.com>
2024-12-07 13:04:45 +0100

    day-7: Done

--- day-7/src/bin/b.rs
+++ day-7/src/bin/b.rs
@@ -1,14 +1,10 @@
-// use flexi_logger;
-// use log::{info, warn};
-
 use tracing_subscriber::{filter, prelude::*};
 use std::{fs::File, sync::Arc};
-use tracing::{info, debug, warn};
+use tracing::{info, debug};
 use std::io::{BufRead, BufReader};
 
 fn setup_tracing() {
-    let stdout_log = tracing_subscriber::fmt::layer()
-        .pretty();
+    let stdout_log = tracing_subscriber::fmt::layer();
 
     // A layer that logs events to a file.
     let file = File::create("debug.log");
@@ -26,10 +22,6 @@ fn setup_tracing() {
                 .and_then(debug_log)
         )
         .init();
-
-    debug!("Debug message example");
-    info!("Info message example");
-    warn!("Warning message example");
 }
 
 fn main() {
@@ -37,15 +29,65 @@ fn main() {
     info!("{:?}", get_answer("input"));
 }
 
+fn calibration_result(expected: usize, acc: usize, rest: &[usize]) -> usize {
+    let _span0 = tracing::span!(tracing::Level::INFO, "", acc).entered();
+    if rest.len() == 0 {
+        if acc == expected {
+            info!("Match!");
+            return expected;
+        } else {
+            debug!("No match");
+            return 0;
+        }
+    }
+
+    if acc > expected {
+        return 0;
+    }
+
+    if acc != 0 {
+        let expected_str = format!("{}", expected);
+        let rest_str = format!("{}{}", acc, rest[0]);
+        if rest_str.len() <= expected_str.len() {
+            let _span1 = tracing::span!(tracing::Level::INFO, "", "|| {}", rest[0]).entered();
+            let comb = calibration_result(expected, rest_str.parse().unwrap(), &rest[1..]);
+            if comb == expected {
+                return expected;
+            }
+        }
+        {
+            let _span1 = tracing::span!(tracing::Level::INFO, "", "x {}", rest[0]).entered();
+            let mult = calibration_result(expected, acc * rest[0], &rest[1..]);
+            if mult == expected {
+                return expected;
+            }
+        }
+    }
+    let _span1 = tracing::span!(tracing::Level::INFO, "", "+ {}", rest[0]).entered();
+    calibration_result(expected, acc + rest[0], &rest[1..])
+}
+
 fn get_answer(file: &str) -> usize {
-    let mut input: Vec<Vec<String>> = BufReader::new(File::open(file).unwrap()).lines();
-    info!("{:?}: Using as input data", file);
-    return 1;
+    let input: Vec<Vec<usize>> = BufReader::new(File::open(file).unwrap()).lines()
+        .filter_map(Result::ok)
+        .map(|line| line
+            .split(&[':', ' '])
+            .filter(|str| !str.is_empty())
+            .map(|val_str| { debug!("{}", val_str); val_str.parse::<usize>().unwrap() }).collect()
+        )
+        .collect();
+
+    let mut total_calibration_result = 0usize;
+    for test in input {
+        let _a = tracing::span!(tracing::Level::INFO, "", "{}: {:?}", test[0], &test[1..]).entered();
+        total_calibration_result += calibration_result(test[0], 0, &test[1..]);
+    }
+
+    total_calibration_result
 }
 
 #[test]
 fn test() {
     setup_tracing();
-    assert_eq!(4, get_answer("test.1"));
-    assert_eq!(8, get_answer("test.2"));
+    assert_eq!(11387, get_answer("test.a"));
 }
