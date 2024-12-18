e2a888bc47350febaf7f7bd1e4a229758bb4518f
Mats Liljegren <liljegren.mats@gmail.com>
2024-12-18 08:56:07 +0100

    Day-18: a: Keeps crashing

---
+++ day-18/src/bin/a.rs
@@ -0,0 +1,139 @@
+use tracing_subscriber::{filter, prelude::*};
+use std::{fs::File, sync::Arc};
+use tracing::{info, debug};
+use std::io::{BufRead, BufReader};
+use itertools::Itertools;
+use rust_tools::grid2d::Grid2D;
+use std::collections::VecDeque;
+
+fn setup_tracing() {
+    let stdout_log = tracing_subscriber::fmt::layer()
+        .pretty();
+
+    // A layer that logs events to a file.
+    let file = File::create("debug.log");
+    let file = match file  {Ok(file) => file,Err(error) => panic!("Error: {:?}",error),};
+    let debug_log = tracing_subscriber::fmt::layer()
+        .with_writer(Arc::new(file));
+
+    tracing_subscriber::registry()
+        .with(
+            stdout_log
+                .with_filter(filter::LevelFilter::INFO)
+                .and_then(debug_log)
+        )
+        .init();
+}
+
+fn main() {
+    setup_tracing();
+    info!("{:?}", get_answer("input", (71, 71), 1024));
+}
+
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
+enum MapItem {
+    #[default]
+    Empty,
+    Corrupted,
+    Step,
+}
+
+impl std::fmt::Display for MapItem {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}", match self {
+            MapItem::Empty => '.',
+            MapItem::Corrupted => '#',
+            MapItem::Step => 'O',
+        })
+    }
+}
+
+fn shortest_path(map: &Grid2D<MapItem>, goal: (usize, usize)) -> VecDeque<(usize,usize)> {
+    let mut paths: Vec<VecDeque<(usize, usize)>> = vec![VecDeque::from(vec![(0,0)])];
+
+    loop {
+        let mut shortest_path: VecDeque<(usize, usize)> = VecDeque::new();
+        let mut shortest: Option<usize> = None;
+        for path in &paths {
+            if let Some(pos) = path.front() {
+                if *pos == goal {
+                    debug!("Path reached goal, length: {}", path.len());
+                    if shortest.is_none() || shortest.unwrap() > path.len() {
+                        shortest = Some(path.len());
+                        shortest_path = path.clone();
+                    }
+                }
+            }
+        }
+        if shortest_path.len() > 0 {
+            return shortest_path;
+        }
+        let mut new_paths: Vec<VecDeque<(usize, usize)>> = Vec::new();
+        let mut remove_paths: Vec<VecDeque<(usize, usize)>> = Vec::new();
+        for path in &mut paths {
+            let curr = *path.front().unwrap();
+            let next = map.successors_with(curr, |_, pos|
+                !path.contains(&pos) && map[pos] != MapItem::Corrupted && (pos.0 == curr.0 || pos.1 == curr.1)
+                );
+            for idx in 1..next.len() {
+                let mut new_path = path.clone();
+                new_path.push_front(next[idx]);
+                new_paths.push(new_path);
+            }
+            if next.len() > 0 {
+                path.push_front(next[0]);
+            } else {
+                remove_paths.push(path.clone());
+            }
+        }
+        for path in remove_paths {
+            for idx in 0..paths.len() {
+                if path == paths[idx] {
+                    paths.remove(idx);
+                    break;
+                }
+            }
+        }
+        if new_paths.len() > 0 {
+            paths.append(&mut new_paths);
+        }
+    }
+}
+
+
+fn get_answer(file: &str, size: (usize, usize), nr_rounds: usize) -> usize {
+    let input: Vec<(usize, usize)> = BufReader::new(File::open(file).unwrap()).lines()
+        .filter_map(Result::ok)
+        .map(|line| line
+            .split(',')
+            .map(|value| value.parse::<usize>().unwrap())
+            .collect_tuple()
+            .unwrap()
+        )
+        .collect::<Vec<_>>();
+
+    let mut map: Grid2D<MapItem> = Grid2D::default();
+    map.set_size(size, MapItem::default());
+
+    for idx in 0..nr_rounds {
+        map[input[idx]] = MapItem::Corrupted;
+    }
+
+    debug!("\n{}", map);
+
+    let path = shortest_path(&map, (size.0-1, size.1-1));
+
+    for step in &path {
+        map[*step] = MapItem::Step;
+    }
+
+    debug!("\n{}", map);
+
+    path.len() - 1
+}
+
+#[test]
+fn test() {
+    setup_tracing();
+    assert_eq!(22, get_answer("test.a", (7, 7), 12));
+}
